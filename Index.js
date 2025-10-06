8ce914f2f0bec80ee2412a28502551ee17776b89
আEMON AHMED RANAাম <emon9996a@gmail.com>
2025-10-06 18:06:32 +0600

    a

---
+++ Index.js
@@ -0,0 +1,95 @@
+//21.
+let name = 'Sarif';
+
+//22. 
+
+let Phone = {type:"IOS", Price:"50000", color:"Blue"};
+
+//23. 
+//Array is type of data which can carry a group of data at a  time. 
+
+let Num = [ 11, 123, 146 ,645 ] 
+
+//24. function is a Js , we can use it to call a veriable for result.
+
+function vatcalculator (Price) {
+  let result = price * 25/100;
+  return result;
+}
+
+let bike = 10000;
+price = vatcalculator (10000);
+console.log(price);
+
+
+
+
+//25. 
+
+for (let i = 1; i <= 20; i++){ 
+  num += i ;
+}
+
+console.log(num);
+
+
+
+//section C. 
+// 26.
+
+let x = 5;
+let y = 5;
+
+console.log( x==y, x===y);
+
+//27. 
+
+let arr = [1, 2, 3];
+arr.pop();     
+arr.push(4);      
+console.log(arr); 
+
+//28. 
+
+let obj = { name: 'John', age: 25}; 
+
+console.log(obj.name);
+
+//29. 
+let numbers = [ 2,4,6,8] ; 
+console.log(numbers[2]);
+
+//30.function sumArray(arr) {
+  let sum = 6 + 2;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
+let text = 'jacvascript'; 
+
+console.log(text.slice(4,10));
+
+
+//Section D.
+//31. 
+
+let num = 1;
+
+if (num  % 2 === 0) {
+    console.log("True");
+} else {
+  console.log("false")
+}
+
+//32.
+function sumArray(arr) {
+  let sum = 6 + 2;
+  for (let num of arr) {
+    sum += num;
+  }
+  return sum;
+}
+//33. 
+
+
